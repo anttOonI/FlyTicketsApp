c784b80973bbbc5ebd98625a462e2cb316c3e1e7
anttOonI <93sant@mail.ru>
2021-07-18 22:13:57 +0300

    Add Favorites screen tableView

---
+++ FlyTickets/FavouritesScreen/View/Cell/FavoriteViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteViewCell.swift
+//  FlyTickets
+//
+//  Created by AntonSobolev on 18.07.2021.
+//
+
+import UIKit
+
+class FavoriteViewCell: UITableViewCell {
+
+	static let identifier = "FavoriteViewCell"
+
+
+}
--- FlyTickets/FavouritesScreen/View/FavoritesVC.swift
+++ FlyTickets/FavouritesScreen/View/FavoritesVC.swift
@@ -13,4 +13,41 @@ class FavoritesVC: UIViewController {
         super.viewDidLoad()
 
     }
+	
+	
+	// MARK: - Private Properties
+	
+	private var tableView = UITableView()
+	
+	// MARK: - Private Methods
+	
+	private func configureTableView() {
+		view.addSubview(tableView)
+		setTableViewDelegates()
+		tableView.register(FavoriteViewCell.self, forCellReuseIdentifier: FavoriteViewCell.identifier)
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+			tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+		])
+	}
+
+	private func setTableViewDelegates() {
+		tableView.delegate = self
+		tableView.dataSource = self
+	}
+}
+
+extension FavoritesVC: UITableViewDelegate, UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 10
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+	
+	
 }
